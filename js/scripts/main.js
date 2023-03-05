c9c24b243f870a214687c5d26dfa6fd564046756
Luciano Silva <legostr@gmail.com>
2023-03-05 17:19:08 -0300

    Menu Suspenso com JavaScript

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -37,3 +37,62 @@ allFilters.forEach((filter, index) => {
     filter.classList.add('active');
   })
 })
+
+const btnOpenModal = document.querySelector('.js-open-modal');
+const btnCloseModal = document.querySelector('.js-close-modal');
+const clickOutside = document.querySelector('.modal');
+const tagHtml = document.documentElement;
+
+
+btnOpenModal.addEventListener('click', (event) => {
+  event.preventDefault();
+  tagHtml.classList.add('show-modal')
+})
+
+btnCloseModal.addEventListener('click', () => {
+  tagHtml.classList.remove('show-modal')
+})
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    tagHtml.classList.remove('show-modal');
+  }
+})
+
+document.addEventListener('click', (event) => {  
+  if(event.target.closest('.js-open-modal')) return
+  if(!event.target.closest('.modal .box')){
+    tagHtml.classList.remove('show-modal')
+  }
+})
+
+const btnMenu = document.querySelectorAll ('.js-btn-menu');
+const menuSite = document.querySelectorAll('.js-menu');
+
+btnMenu.forEach((btn, index) => {
+   btn.addEventListener('click', (event) => {
+      event.preventDefault();
+      
+      menuSite.forEach(itemMenu => {
+        itemMenu.classList.remove('active');
+        
+        itemMenu.addEventListener('mouseleave',() => {
+          itemMenu.classList.remove('active');
+          btnMenu.forEach(itemBtn => {
+            itemBtn.classList.remove('active');
+          })
+        })
+      })
+
+      btnMenu.forEach(itemBtn => {
+        itemBtn.classList.remove('active');
+      })
+
+      btn.classList.add('active');
+      menuSite[index].classList.add('active');
+
+   })
+   
+  })
+
+
